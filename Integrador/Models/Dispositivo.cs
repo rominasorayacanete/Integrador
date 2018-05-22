46db5652fbba3687d27446cd5cc28586954a1d9f
Diegobevilacqua <dfb_98@hotmail.com>
2018-05-21 23:02:08 -0300

    commit clases

--- Integrador/Models/Dispositivo.cs
+++ Integrador/Models/Dispositivo.cs
@@ -7,5 +7,9 @@ namespace Integrador.Models
 {
     public class Dispositivo
     {
+        public Actuador actuador;
+        public Sensor sensor;
+        public float consumoPorHora { get; set; }
+        public string nombreGenerico { get; set; }
     }
 }
