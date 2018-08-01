1c46092ffceeb4ac22cd47fd50fe31b20dc374c5
ledesma.93 <ledesma.93@gmail.com>
2018-08-01 19:19:09 -0300

    simplex service

---
+++ Integrador/Services/HttpService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+using TpPrevio.Models;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Simplex.Services
+{
+    public class HttpService
+    {
+        public List<Dispositivos> GetDispositivos ()
+        {
+            string sUrlRequest = "FALTA QUE MANDEN LA URL";
+            var json = new WebClient().DownloadString(sUrlRequest);
+            var dispositivos = JsonConvert.DeserializeObject<List<Dispositivo>>(json);
+            return dispositivos;
+        }
+
+    }
+}
