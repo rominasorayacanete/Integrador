7f7153d0ef19e340d67e7932d7a3cf22c691f183
Diego Bevilacqua <dfb_98@hotmail.com>
2018-09-12 17:21:19 -0300

    Agregado de clase Administrador

---
+++ Integrador/Models/Clases/Administrador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Integrador.Models.Clases
+{
+    public class Administrador
+    {
+        public DateTime fechaAltaSistema { get; set; }
+        public int idSistema { get; set; }
+
+        public int MesesComoAdministrador()
+        {
+            return Math.Abs((DateTime.Now.Month - fechaAltaSistema.Month) + 12 * (DateTime.Now.Year - fechaAltaSistema.Year));
+        }
+    }
+}
