62dbbaa62b8a3a2fb3f561a8425ba66dbb463c5a
Diego Bevilacqua <dfb_98@hotmail.com>
2018-11-05 16:10:21 -0300

    Parte de Test2

--- Tests unitarios/Test2.cs
+++ Tests unitarios/Test2.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Integrador.ORM;
+using System.Data.Entity;
+using System.Linq;
 
 namespace Tests_unitarios
 {
@@ -9,7 +12,28 @@ namespace Tests_unitarios
         [TestMethod]
         public void TestMethod1()
         {
+            using(DBContext db = new DBContext())
+            {
+                string nombreGrabado = CambiarNombre(db);
+                string nombrePersistido = NombrePersistido(db);
+                Assert.AreEqual(nombreGrabado, nombrePersistido);
+            }
+        }
 
+        public string CambiarNombre(DBContext db)
+        {
+            var query = from d in db.Dispositivo select d;
+            var dispositivo = query.FirstOrDefault();
+            string nombreGrabado = dispositivo.nombre_generico = "NuevoNombre";
+            db.SaveChanges();
+            return nombreGrabado;
+        }
+
+        public string NombrePersistido(DBContext db)
+        {
+            var query = from d in db.Dispositivo select d;
+            var dispositivo = query.FirstOrDefault();
+            return dispositivo.nombre_generico;
         }
     }
 }
