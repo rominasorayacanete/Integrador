8c398f883ef3e641c6a2afc644e0c022c2abec4b
Diego Bevilacqua <dfb_98@hotmail.com>
2018-11-08 00:18:41 -0300

    Actualización de DER

--- Integrador/Models/Usuario.cs
+++ Integrador/Models/Usuario.cs
@@ -4,14 +4,14 @@ namespace Integrador.Models
 {
     public class Usuario
     {
-        protected string Username { get; set; }
-        protected string Password { get; set; }
-        protected string Nombre { get; set; }
-        protected string Apellido { get; set; }
-        protected string Tipo_documento { get; set; }
-        protected int Nro_documento { get; set; }
-        protected DateTime FechaAltaSistema { get; set; }
-        protected string Domicilio { get; set; }
-        protected int Telefono { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Tipo_documento { get; set; }
+        public int Nro_documento { get; set; }
+        public DateTime FechaAltaSistema { get; set; }
+        public string Domicilio { get; set; }
+        public int Telefono { get; set; }
     }
 }
---
+++ Integrador/Models/Zona_Geografica.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Integrador.Models
+{
+    public class Zona_Geografica
+    {
+        public int Radio { get; set; }
+        public string NombreZona { get; set; }
+        public double Latitud { get; set; }
+        public double Longitud { get; set; }
+        public List<Transformador> Transformadores { get; set; }
+        public List<Cliente> Clientes { get; set; }
+
+        public float ConsumoTotal()
+        {
+            float consumoTotal = 0;
+
+            foreach (Transformador t in Transformadores)
+            {
+                consumoTotal += t.EnergiaSuministrada;
+            }
+
+            return consumoTotal;
+        }
+    }
+}
