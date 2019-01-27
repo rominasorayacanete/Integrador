201ec07a6b436f299b093f807f19e8711d9070c6
Diegobevilacqua <dfb_98@hotmail.com>
2019-01-27 01:04:57 -0300

    Cambios en vistas.

---
+++ Integrador/Migrations/201901260406024_Migration.cs
@@ -0,0 +1,16 @@
+namespace Integrador.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Migration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
