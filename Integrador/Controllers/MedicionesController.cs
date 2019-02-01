63011fb2197bd6db1ea4e81a23996cc018c72571
Diegobevilacqua <dfb_98@hotmail.com>
2019-02-01 01:32:11 -0300

    Cambios en navbar del home de cliente.

---
+++ Integrador/Controllers/MedicionesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Integrador.Controllers
+{
+    public class MedicionesController : Controller
+    {
+        // GET: Mediciones
+        public ActionResult Index()
+        {
+            return View("~/Views/Estado_Hogar/Index.cshtml");
+        }
+    }
+}
---
+++ Integrador/Controllers/ReglaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Integrador.Controllers
+{
+    public class ReglaController : Controller
+    {
+        // GET: Regla
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
